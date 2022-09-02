9b7a9342c316d68081fe4100dfdd5a8e64334ce6
Marina <umarishka@bk.ru>
2022-09-02 16:57:16 +0300

    add N25

---
+++ N25/Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine("Введите число : ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень числа : ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(Math.Pow(a, b));
